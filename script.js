5254a2f0cd5c12d3b6a1d872c72cb282a0d0fb3d
aninditasarkar10 <96198066+aninditasarkar10@users.noreply.github.com>
2023-06-19 20:14:51 +0530

    added hgml

---
+++ script.js
@@ -0,0 +1,80 @@
+const currentTime = document.querySelector("h1"),
+selectMenu = document.querySelectorAll("select"),
+content= document.querySelector(".content"),
+setAlarmBtn= document.querySelector("button");
+let alarmTime,isAlarmSet=false;
+ringtone= new Audio("alarm.mp3");
+
+for(i=12;i>0;i--){
+    i=i<10 ?"0"+i:i;
+    let option= `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend",option);
+
+
+}
+ 
+for(i=59;i>0;i--){
+    i=i<10 ?"0"+i:i;
+    let option= `<option value="${i}">${i}</option>`;
+    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend",option);
+
+
+}
+ 
+for(i=2;i>0;i--){
+ let ampm=i==1?"AM":"PM"
+    let option= `<option value="${ampm}">${ampm}</option>`;
+    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend",option);
+
+
+}
+
+setInterval(()=>
+{
+    let date=new Date(),
+    h =date.getHours(),
+    m =date.getMinutes(),
+    s=date.getSeconds(),
+    ampm="AM";
+ if(h>=12)
+ {
+    h=h-12;
+    ampm="PM";
+
+ }
+ h=h==0?h=12:h;
+ h=h<10?"0"+h:h;
+ m=m<10?"0"+m:m;
+ s=s<10?"0"+s:s;
+ currentTime.innerText=`${h}:${m}:${s} ${ampm}`;
+ if(alarmTime ==`${h}:${m} ${ampm}`){
+    //console.log("alarm ringing..")
+    ringtone.play();
+    ringtone.loop = true;
+    
+ }
+},1000);
+
+function setAlarm(){
+    if(isAlarmSet){
+       alarmTime="";//clear the value
+        ringtone.pause();//pause the ring
+        content.classList.remove("disable");
+        setAlarmBtn.innerText="set Alarm";
+         return isAlarmSet=false;//if false return
+    
+
+    }
+    //get hour min select tag value 
+    let time=`${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+    if(time.includes("Hour")||time.includes("minute") ||time.includes("AM/PM") ){
+        return alert("please,select a valid time to set alarm!");
+    }
+    //isAlarmSet=true;
+    alarmTime=time;
+    content.classList.add("disable");
+    setAlarmBtn.innerText ="clear Alarm";
+
+}
+setAlarmBtn.addEventListener("click",setAlarm);
+ 
